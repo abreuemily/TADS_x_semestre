e160c0761484f8fd66ce93bdfe105994b6f451f7
abreuemily <almeidaabreuemily@gmail.com>
2023-03-06 12:35:49 -0300

    atualizações poo

--- Linguagem de programação e orientação a objetos/questao43/src/questao43/Main.java
+++ Linguagem de programação e orientação a objetos/questao43/src/questao43/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		// 43. Faça um programa que leia um número de 0 e 60 e exiba na tela o seu sucessor, 
 		//sabendo que o sucessor de 60 a 0. Não pode ser utilizado nem um comando de seleção e repetição
 		int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe um número de 0 a 60:"));
-		int sus = (numero + 1) % 62;
+		int sus = (numero + 1) % 61;
 		JOptionPane.showMessageDialog(null, String.format("O sucessor de %d é %d.", numero, sus));
 	}
 
