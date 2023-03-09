4b112820f799a93ecb3049e5086ef28e2cf957a2
abreuemily <almeidaabreuemily@gmail.com>
2023-03-09 17:41:49 -0300

    9/3 metodos quantitativos

--- metodos quantitativos/matriz.R
+++ metodos quantitativos/matriz.R
@@ -120,3 +120,55 @@ DD
 # DD / (n - 1) = Var-Cor
 S <- DD*(1/(n-1)) # Var-Cov
 S
+
+
+
+# REGRESSÃO NO R --- aula do dia 9/3
+# regressão com o uso da função lm()
+#Lendo os dados
+#dados de rendimento (%) de um produto quimico e temperatura de produto
+Rend <- c(60, 70, 77, 86, 91)
+Temp <- c(40, 45, 50, 55, 60)
+plot(Rend ~ Temp)
+#Rodando regressão lm(y~x)
+
+#modelo 1
+
+lm(Rend ~ Temp)
+mod1 <- lm(Rend ~ Temp) #Regressão
+summary(mod1) #resultados
+names(mod1) # nomes dos componentes da regressão
+mod1$coefficients
+mod1$coefficients[2]
+mod1$residuals
+mod1$fitted.values
+plot(mod1$fitted.values~Temp, type='l',col = 'blue')
+points(Rend~Temp, col = 'red', pch = 19)
+
+
+#modelo 2
+Rend2 <- c(24, 40, 60, 70, 77, 86, 91, 86, 84)
+Temp2 <- c(30, 35, 40, 45, 50, 55, 60, 65, 70)
+ls()
+plot(Rend2 ~ Temp2)
+mod2 <- lm(Rend2~Temp2)
+summary(mod2)
+mod2$residuals
+plot(mod2$residuals~Temp2)
+abline(h=0, lty='dashed')
+plot(mod1$residuals~Temp)
+abline(h=0, lty='dashed')
+
+#modelo 3
+# modelo 3: com x^2
+#regressão precisa I(Temp^2)
+
+mod3 <- lm(Rend2~Temp2+I(Temp2^2))
+summary(mod3)
+
+mod3$residuals
+plot(mod3$residuals~Temp2)
+abline(h=0, lty='dashed')
+
+plot(Rend2~Temp2)
+lines(Temp2, mod3$fitted.values, col='red')
