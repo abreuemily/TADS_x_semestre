e64112cc4390cbb0dd2c2743edbb46b1d16007e4
abreuemily <almeidaabreuemily@gmail.com>
2023-03-21 17:57:00 -0300

    atualizações poo 21/3

--- Linguagem de programação e orientação a objetos/orientacao_a_objetos/src/orientacao_a_objetos/Aluno.java
+++ Linguagem de programação e orientação a objetos/orientacao_a_objetos/src/orientacao_a_objetos/Aluno.java
@@ -2,9 +2,67 @@ package orientacao_a_objetos;
 
 public class Aluno 
 {
-	String nome;
-	int idade; 
-	int cpf;
-	boolean menor;
+	private String nome;
+	private int idade; 
+	private int cpf;
+	private boolean menor;
+	
+	public void setNome(String nome) 
+	{
+		this.nome = nome; //se a variável local e a de instância tiverem o mesmo nome, para identificar coloca-se a palavra this. na variavel de instância
+		
+	}
+	
+	public String getNome() 
+	{
+		
+		return nome;
+	}
+	
+	public void setIdade(int n) 
+	{
+		if (n > 100)
+			idade = 0;
+		else
+			idade = n;
+		
+	}
+	
+	public int getIdade() 
+	{
+		return idade;
+		
+	}
+	
+	public void setCpf(int n) 
+	{
+		cpf = n;
+		
+	}
+	
+	public int getCpf() 
+	{
+		return cpf;
+		
+	}
+	
+	public void setMenor(boolean n) 
+	{
+		menor = n;
+		
+	}
+	
+	public boolean getMenor() 
+	{
+		return menor;
+		
+	}
+
+	//se as variáveis de instância estivessem aqui, ainda fariam parte da classe
 
 }
+
+//níveid de acesso a uma variável: public, private, protected, default
+// inserir, alterar, deletar dado da variável -> set  consultar dado da variável -> get
+// para gerar os métodos getter e setter automaticamente, vá em Source -> Generate getters and setters -> marque as variáveis desejadas -> Generate
+	
--- Linguagem de programação e orientação a objetos/orientacao_a_objetos/src/orientacao_a_objetos/AlunoExe.java
+++ Linguagem de programação e orientação a objetos/orientacao_a_objetos/src/orientacao_a_objetos/AlunoExe.java
@@ -1,24 +1,30 @@
 package orientacao_a_objetos;
 
+import javax.swing.JOptionPane;
+
 public class AlunoExe 
 {
 	public static void main(String[] args) 
 	{
 		Aluno a1; //variável de referencia, diz onde está o objeto
 		a1 = new Aluno();
-		
+		Aluno a3;
 		Aluno a2 = new Aluno();
+		a3 = a1;
+		a1.setIdade(Integer.parseInt(JOptionPane.showInputDialog(null, "informe a idade:")));
+		JOptionPane.showMessageDialog(null, a1.getIdade());
+		//a1.nome = "Emily";
+		//a1.cpf = 309396580;
+		//a1.idade = 21;
+		//a1.menor = false;
 		
-		a1.nome = "Emily";
-		a1.cpf = 309396580;
-		a1.idade = 21;
-		a1.menor = false;
-		
-		a2.nome = "Ylime"; 
-		a2.cpf = 876587578;
-		a2.idade = 12;
-		a2.menor = true;
-		
+		//a2.nome = "Ylime"; 
+		//a2.cpf = 876587578;
+		//a2.idade = 12;
+		//a2.menor = true;
+		//a2 = null;
+		a1 = null;
+		//a1.nome = "Abreu";
 	}
 
 }
